2e2f4e2913db78b75993c4ae84c87d17356eb2a7
bananpalme <nigginz12@gmail.com>
2023-03-19 20:07:32 +0100

    menuprojekt hooray!

---
+++ sveltejs-menu/src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+
+	class MyMenu extends HTMLElement {
+		constructor() {
+			super();
+			this.attachShadow({ mode: 'open' });
+			this.shadowRoot.innerHTML = `
+			<style>
+				/* CSS-styling til webkomponenten */
+				</style>
+				<ul>
+					<li><a href="#">Item 1</a></li>
+					<li><a href="#">Item 2</a></li>
+					<li><a href="#">Item 3</a></li>
+					</ul>
+					`;
+				}
+			}
+			
+			customElements.define('my-menu', MyMenu);
+</script>
